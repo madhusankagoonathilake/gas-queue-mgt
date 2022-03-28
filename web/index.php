aa6eaceef678b641339a5b82848b6c062d80fd6a
Rukshan <arkruka@gmail.com>
2022-03-28 12:12:25 +0530

    feat: add english laguage to front page, and laguage selection

--- web/index.php
+++ web/index.php
@@ -8,6 +8,26 @@ if (isLoggedIn()) {
     header('Location: /agency-dashboard.php');
 }
 
+if ($_POST['language']) {
+    if ($_POST['language'] == 'si') {
+        setcookie('language', 'si', time() + (86400 * 30), "/");
+    } else if ($_POST['language'] == 'en') {
+        setcookie('language', 'en', time() + (86400 * 30), "/");
+    }
+}
+
+if (!isset($_COOKIE['language'])) {
+    setcookie('language', 'si', time() + (86400 * 30), "/");
+}
+
+if (isset($_COOKIE['language']) && $_COOKIE['language'] == 'si') {
+    $line1 = "ඒජන්සි හිමියෙක්මි";
+    $line2 = "ගනුම්කරුවෙක්මි";
+} else if (isset($_COOKIE['language']) && $_COOKIE['language'] == 'en') {
+    $line1 = "Agency Owner";
+    $line2 = "Buyer";
+}
+
 include_once '../templates/header.php';
 ?>
 
@@ -15,10 +35,18 @@ include_once '../templates/header.php';
     <h1>මම</h1>
     <p class="lead">&nbsp;</p>
     <p class="lead">
-        <a href="agency.php" class="btn btn-lg btn-secondary fw-bold border-primary bg-primary text-white m-3">ඒජන්සි
-            හිමියෙක්මි</a>
-        <a href="buyer.php" class="btn btn-lg btn-secondary fw-bold border-success bg-success text-white m-3">ගැණුම්කරුවෙක්මි</a>
-    </p>
+        <a href="agency.php" class="btn btn-lg btn-secondary fw-bold border-primary bg-primary text-white m-3"><?php echo $line1; ?></a>
+        <a href="buyer.php" class="btn btn-lg btn-secondary fw-bold border-success bg-success text-white m-3"></a>
+    </p>   
+    <h2>කරුණාකර ඔබගේ භාෂාව තෝරන්න | Please select your language</h2>
+    <form action="/" method="post">
+        <label for="language">භාෂාව | Language</label>
+        <select name="language" id="language" class="form-select">
+            <option value="si">සිංහල | Sinhala</option>
+            <option value="en">ඉංග්‍රීසි | English</option>
+        </select>
+        <button type="submit" class="btn btn-success">භාෂාව තෝරන්න | Select Language</button>
+    </form>
 </main>
 
 <?php include_once '../templates/footer.php' ?>
